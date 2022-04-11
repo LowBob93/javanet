624edd8644c7dad08395b9e8867f96deecdc6eb1
LowBob93 <Tester269@gmail.com>
2022-04-11 19:13:31 +0300

    second commit

--- client/src/main/java/ru/gb/storage/client/Client.java
+++ client/src/main/java/ru/gb/storage/client/Client.java
@@ -1,14 +1,65 @@
 package ru.gb.storage.client;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class Client extends Application {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class Client {
+    private final static ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final static Scanner SC = new Scanner(System.in);
+    private static SocketChannel SOCKET;
+    static ByteBuffer bf = ByteBuffer.allocate(1024);
+
     public static void main(String[] args) {
-        Application.launch(args);
-    }
 
-    public void start(Stage stage) throws Exception {
-        stage.show();
+        try {
+            ecoClient();
+        } finally {
+            SC.close();
+        }
+    }
+    public static void ecoClient() {
+        try {
+            SOCKET = SocketChannel.open(new InetSocketAddress("localhost", 8080));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Client started");
+        executorService.execute(()->{
+            read();
+        });
+        readServerMassage();
+    }
+    public static void readServerMassage() {
+        while (true) {
+            String msg = SC.nextLine();
+            try {
+                SOCKET.write(ByteBuffer.wrap(msg.getBytes()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Server Send Massage: " + msg);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void read () {
+        try {
+            SOCKET.read(bf);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String message = new String(bf.array());
+        System.out.println("Message from server: " + message);
     }
 }
--- server/src/main/java/ru/gb/storage/server/Server.java
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -1,7 +1,79 @@
 package ru.gb.storage.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Server {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        new Server().start();
+    }
+
+    private void start() throws IOException {
+        Selector selector = Selector.open();
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress("localhost", 8080));
+        serverSocketChannel.configureBlocking(false);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
         System.out.println("Server started");
+
+        while(true){
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()){
+                SelectionKey selectionKey = iterator.next();
+                if (selectionKey.isAcceptable()) {
+                    System.out.println("New selector acceptable event");
+                    register(selector, serverSocketChannel);
+                }
+
+                if (selectionKey.isReadable()) {
+                    System.out.println("New selector readable event");
+                    readMessage(selectionKey);
+                }
+                iterator.remove();
+            }
+        }
+    }
+    private void register(Selector selector, ServerSocketChannel serverSocketChannel) throws IOException {
+        SocketChannel client = serverSocketChannel.accept();
+        client.configureBlocking(false);
+
+        client.register(selector, SelectionKey.OP_READ,  new StringBuilder());
+        System.out.println("New client is connected");
+    }
+    private void readMessage(SelectionKey selectionKey) throws IOException {
+        SocketChannel client = (SocketChannel) selectionKey.channel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        client.read(byteBuffer);
+        byteBuffer.flip();
+        byte[] bytes = new byte[byteBuffer.limit()];
+        byteBuffer.get(bytes);
+        String message = new String(bytes);
+        System.out.println("New message: " + message.trim());
+        if(selectionKey.attachment() != null) {
+            StringBuilder sb = (StringBuilder) selectionKey.attachment();
+            sb.append(message.trim());
+            if (message.endsWith("\n")) {
+                sb.append("\n");
+                byte[] sendBytes = sb.toString().getBytes();
+                ByteBuffer sendBuffer = ByteBuffer.allocate(sendBytes.length);
+                sendBuffer.put(sendBytes);
+                sendBuffer.flip();
+                client.write(sendBuffer);
+                sb.setLength(0);
+            }
+        } else {
+            client.write(ByteBuffer.wrap(message.getBytes()));
+        }
     }
 }
