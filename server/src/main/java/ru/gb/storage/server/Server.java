a30e32cbf2602649aaffda7b3747048e69aaf92b
LowBob93 <Tester269@gmail.com>
2022-04-14 18:25:58 +0300

    netty server with /n check commit

--- server/src/main/java/ru/gb/storage/server/Server.java
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -1,79 +1,65 @@
 package ru.gb.storage.server;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.util.Iterator;
-import java.util.Set;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class Server {
+    private final int port;
 
-    public static void main(String[] args) throws IOException {
-        new Server().start();
+    public Server(int port) {
+        this.port = port;
     }
 
-    private void start() throws IOException {
-        Selector selector = Selector.open();
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.socket().bind(new InetSocketAddress("localhost", 8080));
-        serverSocketChannel.configureBlocking(false);
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        System.out.println("Server started");
-
-        while(true){
-            selector.select();
-            Set<SelectionKey> selectionKeys = selector.selectedKeys();
-            Iterator<SelectionKey> iterator = selectionKeys.iterator();
-            while (iterator.hasNext()){
-                SelectionKey selectionKey = iterator.next();
-                if (selectionKey.isAcceptable()) {
-                    System.out.println("New selector acceptable event");
-                    register(selector, serverSocketChannel);
-                }
-
-                if (selectionKey.isReadable()) {
-                    System.out.println("New selector readable event");
-                    readMessage(selectionKey);
-                }
-                iterator.remove();
-            }
-        }
+    public static void main(String[] args) throws InterruptedException {
+        new Server(8080).start();
     }
-    private void register(Selector selector, ServerSocketChannel serverSocketChannel) throws IOException {
-        SocketChannel client = serverSocketChannel.accept();
-        client.configureBlocking(false);
 
-        client.register(selector, SelectionKey.OP_READ,  new StringBuilder());
-        System.out.println("New client is connected");
-    }
-    private void readMessage(SelectionKey selectionKey) throws IOException {
-        SocketChannel client = (SocketChannel) selectionKey.channel();
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-        client.read(byteBuffer);
-        byteBuffer.flip();
-        byte[] bytes = new byte[byteBuffer.limit()];
-        byteBuffer.get(bytes);
-        String message = new String(bytes);
-        System.out.println("New message: " + message.trim());
-        if(selectionKey.attachment() != null) {
-            StringBuilder sb = (StringBuilder) selectionKey.attachment();
-            sb.append(message.trim());
-            if (message.endsWith("\n")) {
-                sb.append("\n");
-                byte[] sendBytes = sb.toString().getBytes();
-                ByteBuffer sendBuffer = ByteBuffer.allocate(sendBytes.length);
-                sendBuffer.put(sendBytes);
-                sendBuffer.flip();
-                client.write(sendBuffer);
-                sb.setLength(0);
-            }
-        } else {
-            client.write(ByteBuffer.wrap(message.getBytes()));
+    public void start() throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) {
+                            ch.pipeline().addLast(
+                                    new ChannelInboundHandlerAdapter() {
+                                        private StringBuilder sb = new StringBuilder();
+
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                            ByteBuf m = (ByteBuf) msg;
+                                            byte[] b = new byte[m.readableBytes()];
+                                            m.readBytes(b);
+                                            String message = new String(b);
+                                            System.out.println("New message: " + message);
+                                            sb.append(message);
+                                            if (message.endsWith("\n")) {
+                                                ((ByteBuf) msg).writeBytes(sb.toString().getBytes());
+                                                ctx.writeAndFlush(msg);
+                                                sb.setLength(0);
+                                            }
+                                        }
+                                    }
+                            );
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture future = server.bind(port).sync();
+            System.out.println("Server Started");
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
         }
     }
 }
